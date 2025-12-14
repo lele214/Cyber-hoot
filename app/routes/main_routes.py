1e64ab4273c064eb750b9e7ca586bc6262a60c3b
Krystiinal <krystiinal.spy@gmail.com>
2025-12-14 20:45:48 +0100

    ajout de la page "quiz" pour commencer

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -6,3 +6,8 @@ main_bp = Blueprint("main", __name__)
 @main_bp.get("/")
 def home():
     return render_template("index.html")
+
+
+@main_bp.get("/quiz/quiz1")
+def quiz1():
+    return render_template("quiz/quiz1.html")
